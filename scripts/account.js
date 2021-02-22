07efccbb00079c9d91c21f8f38103e513d0c9afe
Tim <virtualxself@gmail.com>
2021-02-22 19:45:21 +0200

    Внёс правки в скрипт аккаунта, поправил форму в корзине, сделал секции на главной ссылками

--- scripts/account.js
+++ scripts/account.js
@@ -62,17 +62,17 @@ const profileSwitcher = document.querySelector('#switcher-profile');
 const ordersSwitcher = document.querySelector('#switcher-orders');
 
 function showProfile() {
-  setTimeout(() => {
+
     profile.classList.remove('profile_hidden');
     orders.classList.add('acc-orders_hidden');
-  }, 100)
+
 }
 
 function showOrders() {
-  setTimeout(() => {
+
     profile.classList.add('profile_hidden');
     orders.classList.remove('acc-orders_hidden');
-  }, 100)
+
 }
 
 profileSwitcher.addEventListener('click', () => showProfile());
